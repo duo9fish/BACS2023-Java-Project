e7da931551aae0025dc2f4f9e80e4671f4007013
CRAZJOKER118 <yewchunchung118@gmail.com>
2022-09-27 22:42:29 +0800

    changed some

--- src/Ticket.java
+++ src/Ticket.java
@@ -2,7 +2,7 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Ticket extends Movie {
+public class Ticket {  // remove extend 
     private String movieName;
 
     public Ticket(String movieName) {
@@ -12,15 +12,15 @@ public class Ticket extends Movie {
     public Ticket() {
     }
 
-    public void printTicket(int a, double b) {
+    public void printTicket(int adultQty, double adultPrice) {
         SimpleDateFormat formattor = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         Date date = new Date();
         Theatre ticket = new Theatre();
         for (int i = 0; i < 5; i++) {
             System.out.println("=================================================\n");
             System.out.print("Seat: " + ticket.takenSeats.get(i));
-            System.out.print("Movie: " + getMovieName());
-            System.out.print("Price: RM " + b);
+            System.out.print("Movie: " + movieName); //changed to movieName
+            System.out.print("Price: RM " + adultPrice);  // changed to proper variable name
             System.out.print(formattor.format(date));
             System.out.println("=================================================\n");
         }
